aa716ca8f7868e38cd14542115a8fb43806c595e
Timo Mämecke <timo@maemecke.com>
2017-09-20 00:06:07 +0200

    Add Icon component, currently unused

---
+++ app/src/components/Icon.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Icon = props => {
+  const {
+    icon,
+    width = 24,
+    height = 24,
+    style
+  } = props
+
+  return (
+    <svg style={style} xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox={icon.viewBox}>
+      {icon.path}
+    </svg>
+  )
+}
+
+Icon.propTypes = {
+  icon: PropTypes.string.isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number
+}
+
+export default Icon
