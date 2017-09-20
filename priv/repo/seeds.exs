12caa73182ab129efab5f26b252b6d7f54fb0ee9
Timo Mämecke <timo@maemecke.com>
2017-09-20 10:13:33 +0200

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Beam.Repo.insert!(%Beam.Projects.Project{
+  name: "timomeh/beam",
+  root_directory: "/Users/timomaemecke/Documents/dev/beam/"
+})
