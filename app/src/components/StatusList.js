9f0edcd3c5c8dc4e77386fa8ad58f22e6a8bbbf8
Timo Mämecke <timo@maemecke.com>
2017-09-21 10:12:04 +0200

    Add fancy reorder animations

--- app/src/components/StatusList.js
+++ app/src/components/StatusList.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Div } from 'glamorous'
+import FlipMove from 'react-flip-move'
+import slug from 'slug'
 
 const StatusList = props => {
   const { items, renderItem } = props
 
   return (
-    <div>
+    <FlipMove duration={130} easing="ease-in-out">
       {items.map(item => (
-        <Div marginTop={8} key={item.id}>
+        <Div marginTop={8} key={slug(item.type)}>
           {renderItem(item)}
         </Div>
       ))}
-    </div>
+    </FlipMove>
   )
 }
 
