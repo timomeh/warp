f01e60570ac67ddd4b7be63302d8fd305a244708
Timo Mämecke <timo@maemecke.com>
2017-09-28 10:05:01 +0200

    Add prototype build detail status

--- app/src/containers/Build.js
+++ app/src/containers/Build.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import { fetchProject, fetchBuild } from 'lib/store'
+import { statusColors } from 'bits/styles'
 import Socket from 'lib/socket'
 import ProjectHeader from 'components/ProjectHeader'
 import BuildOverview from 'components/BuildOverview'
@@ -31,7 +32,8 @@ class Project extends Component {
       build: { selectedId: buildId, isFetching: buildIsFetching },
       projects,
       builds,
-      pipelines
+      pipelines,
+      stages
     } = this.props
 
     if (projectIsFetching || buildIsFetching) return <div>Loading</div>
@@ -50,8 +52,35 @@ class Project extends Component {
           build={build}
           pipeline={pipeline}
         />
+        {build.stages.map(stageId => {
+          const stage = stages.entities[stageId]
+
+          return <ul>
+            <li style={{color: statusColors[stage.status]}}>
+              Stage: {stage.title} | {stage.status} | Start: {stage.started_at} | End: {stage.finished_at}<br />
+            </li>
+            {this.renderSub(stage.steps)}
+          </ul>
+        })}
       </div>
     )
+  }
+
+  renderSub = (stepIds) => {
+    const { steps } = this.props
+
+    return <ul>
+      {stepIds.map(id => {
+        const step = steps.entities[id]
+
+        return <ul>
+          <li style={{marginTop: 20, color: statusColors[step.status]}}>
+            Step: {step.title} | {step.status} | Start: {step.started_at} | End: {step.finished_at}<br />
+          </li>
+          {step.steps && this.renderSub(step.steps)}
+        </ul>
+      })}
+    </ul>
   }
 }
 
